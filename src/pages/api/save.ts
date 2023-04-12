ac8d444d6656e8c4213edc0953c3c968b9e430d2
Stéphane <s.richin@oodrive.com>
2023-04-12 11:10:13 +0200

    fix: using project path for writing files

--- src/pages/api/save.ts
+++ src/pages/api/save.ts
@@ -1,7 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs/promises";
 import Cors from "cors";
+import path from "path";
 import { runMiddleware } from "@/utils/runMiddleware";
+import { writeFile } from "fs/promises";
 
 const cors = Cors({
   methods: ["POST"],
@@ -19,11 +20,9 @@ export default async function handler(
   await runMiddleware(req, res, cors);
 
   const code = Math.floor(Math.random() * 900000);
+  const file = path.join(process.cwd(), "data", `${code}.json`);
 
-  await fs.mkdir(`${__dirname}/data`, { recursive: true });
-
-  const filePath = `${__dirname}/data/${code}.json`;
-  await fs.writeFile(filePath, JSON.stringify(req.body), "utf8");
+  await writeFile(file, JSON.stringify(req.body), "utf8");
 
   res.status(200).json({ code });
 }
--- src/pages/api/sync/[code].ts
+++ src/pages/api/sync/[code].ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs/promises";
+import { readFile } from "fs/promises";
 import path from "path";
 import Cors from "cors";
 import { runMiddleware } from "@/utils/runMiddleware";
@@ -15,7 +15,7 @@ export default async function handler(
 ) {
   await runMiddleware(req, res, cors);
 
-  const filePath = `${path.join(__dirname, "../")}/data/${req.query.code}.json`;
-  const data = await fs.readFile(filePath, "utf8");
+  const file = path.join(process.cwd(), "data", `${req.query.code}.json`);
+  const data = await readFile(file, "utf8");
   res.send(data);
 }
