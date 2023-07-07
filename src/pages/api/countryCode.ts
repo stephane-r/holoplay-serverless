9eb0d5ba64bb4495265f4f1fbc9b14c444dfaf45
Stéphane <s.richin@oodrive.com>
2023-07-07 14:33:14 +0200

    fix: use vercel geo object for get country code

--- src/pages/api/countryCode.ts
+++ src/pages/api/countryCode.ts
@@ -1,9 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Cors from "cors";
 import { runMiddleware } from "@/utils/runMiddleware";
-import { cache } from "@/utils/cache";
-// @ts-ignore
-import { lookup } from "geoip-lite";
 
 const cors = Cors({
   methods: ["GET"],
@@ -14,14 +11,20 @@ type Data = {
   countryCode: string | null;
 };
 
+type Geo = {
+  city?: string;
+  country?: string;
+  region?: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   await runMiddleware(req, res, cors);
 
-  const ip = req.headers["x-forwarded-for"];
-  const geoip = lookup(ip);
+  // @ts-ignore
+  const geo = req.geo as Geo;
 
-  res.status(200).json({ countryCode: geoip?.country ?? null });
+  res.status(200).json({ countryCode: geo?.region ?? null });
 }
