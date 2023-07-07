39ac9b931b0463418b09a66d9db27a9f3a92141b
Stéphane <s.richin@oodrive.com>
2023-07-07 15:14:22 +0200

    fix: update cors for edge function

--- src/pages/api/countryCode.ts
+++ src/pages/api/countryCode.ts
@@ -1,31 +1,26 @@
 import { geolocation, ipAddress } from "@vercel/edge";
-import Cors from "cors";
-import { runMiddleware } from "@/utils/runMiddleware";
-
-const cors = Cors({
-  methods: ["GET"],
-  origin: "*",
-});
+import cors from "../../lib/cors";
 
 export const config = {
   runtime: "edge",
 };
 
 export default async function handler(request: Request, response: Response) {
-  // @ts-ignore
-  await runMiddleware(request, response, cors);
   // The geolocation helper pulls out the geoIP headers from the
   const geo = geolocation(request) || {};
   // The IP helper does the same function for the user's IP address
   const ip = ipAddress(request) || null;
 
-  return new Response(
-    JSON.stringify({
-      ...geo,
-      ip,
-    }),
-    {
-      headers: { "content-type": "application/json" },
-    }
+  return cors(
+    request,
+    new Response(
+      JSON.stringify({
+        ...geo,
+        ip,
+      }),
+      {
+        headers: { "content-type": "application/json" },
+      }
+    )
   );
 }
