7b3cc76a31b2440ca7d5a97a79686d8d352e21cd
Stéphane <s.richin@oodrive.com>
2023-07-07 15:06:32 +0200

    fix: add missing cors

--- src/pages/api/countryCode.ts
+++ src/pages/api/countryCode.ts
@@ -1,10 +1,19 @@
 import { geolocation, ipAddress } from "@vercel/edge";
+import Cors from "cors";
+import { runMiddleware } from "@/utils/runMiddleware";
+
+const cors = Cors({
+  methods: ["GET"],
+  origin: "*",
+});
 
 export const config = {
   runtime: "edge",
 };
 
-export default async function handler(request: Request) {
+export default async function handler(request: Request, response: Response) {
+  // @ts-ignore
+  await runMiddleware(request, response, cors);
   // The geolocation helper pulls out the geoIP headers from the
   const geo = geolocation(request) || {};
   // The IP helper does the same function for the user's IP address
