6bb42b8131bfb0b0d72ee4c8b46263d266d02628
Stéphane <s.richin@oodrive.com>
2023-07-12 17:34:51 +0200

    feat: add sponsorblock-api

---
+++ src/pages/api/sponsorBlock.ts
@@ -0,0 +1,44 @@
+import { SponsorBlock, Category, Constants } from "sponsorblock-api";
+import Cors from "cors";
+import { runMiddleware } from "@/utils/runMiddleware";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const cors = Cors({
+  methods: ["POST"],
+  origin: "*",
+});
+
+type Data = {
+  segments?: any;
+  error?: string;
+};
+
+interface Query {
+  deviceId: string;
+  videoId: string;
+  categories?: Category[];
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await runMiddleware(req, res, cors);
+
+  if (!req.query.deviceId || !req.query.videoId) {
+    res.status(400).json({ error: "Missing deviceId or videoId" });
+  }
+
+  try {
+    const sponsorBlock = new SponsorBlock(req.query.deviceId as string);
+
+    const segments = await sponsorBlock.getSegments(
+      req.query.videoId as string,
+      (req.query.categories as Category[]) ?? Constants.ALL_CATEGORIES
+    );
+
+    res.status(200).json({ segments });
+  } catch {
+    res.status(200).json({ segments: [] });
+  }
+}
